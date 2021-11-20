fac6c57e3111470ddd96cf5c66bc1b83a550f392
Tore Aurstad <tore.aurstad@hemit.no>
2021-11-20 21:58:47 +0100

    Fixes for the dialog window. Closing dialog and property changed notification

--- ToolWindows/ChoosePartialView.xaml.cs
+++ ToolWindows/ChoosePartialView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.PlatformUI;
+using ToreAurstadIT.Razor.Navigate.ToolWindows;
 
 namespace ToreAurstadIT.Razor.Navigate
 {
@@ -9,8 +10,20 @@ namespace ToreAurstadIT.Razor.Navigate
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        private void CloseButton_Click(object sender, System.Windows.RoutedEventArgs e)
         {
+            //Binding in WPF needs help from procedural code.  trying without ! 
+            string selectedCandidateFile = this.lstBoxCandiateFiles.SelectedValue?.ToString();
+            if (!string.IsNullOrEmpty(selectedCandidateFile))
+            {
+                //var vm = (ChoosePartialViewModel)this.DataContext;
+                //vm.CandidateFile = vm.CandidateFile;
+                this.Close();
+            }
+            else
+            {
+                MessageDialog.Show("ToreAurstadIT.Razor.Navigate", "Please select a file first. (Or close this dialog selecting the 'X' button.)", MessageDialogCommandSet.Ok);
+            }
 
         }
     }
