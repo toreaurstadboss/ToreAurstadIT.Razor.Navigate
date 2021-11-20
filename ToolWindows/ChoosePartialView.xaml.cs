f4077dfaad60d84d5a84864d734d0b825a3ea094
Tore Aurstad <tore.aurstad@hemit.no>
2021-11-20 02:15:56 +0100

    More work on the extension. TODO:The published extension on Marketplace does not close the toolbox window?

--- ToolWindows/ChoosePartialView.xaml.cs
+++ ToolWindows/ChoosePartialView.xaml.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.VisualStudio.PlatformUI;
-using System.Windows;
-using ToreAurstadIT.Razor.Navigate.ToolWindows;
 
 namespace ToreAurstadIT.Razor.Navigate
 {
@@ -9,31 +7,11 @@ namespace ToreAurstadIT.Razor.Navigate
         public ChoosePartialView()
         {
             InitializeComponent();
-            this.Loaded += ChoosePartialView_Loaded;
         }
 
-        private void ChoosePartialView_Loaded(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            if (this.lstBoxCandidateFiles.Items.Count > 0)
-            {
-                this.lstBoxCandidateFiles.SelectedIndex = 0; //auto select for now the first candidate file for simplicity - TODO : fix up INotifyPropertyChanged 
-            }
-        }
 
-        private void CloseButtonClick(object sender, System.Windows.RoutedEventArgs e)
-        {
-            string selectedCandidateFile = lstBoxCandidateFiles.SelectedValue?.ToString();
-            var vm = this.DataContext as ChoosePartialViewModel;
-            if (vm != null)
-            {
-                vm.CandidateFile = selectedCandidateFile;
-            }
-            if (string.IsNullOrEmpty(selectedCandidateFile))
-            {
-                MessageBox.Show("Select file first.");
-                return;
-            }
-            this.Close();
         }
     }
 }
